4fcb6f0d00c90355b50e02880c56d260cdc1d387
vignesh <vigneshbalajiee15@gmail.com>
2024-08-25 23:16:06 +0530

    Terraform creating EC2 instance

---
+++ Terraform/Top level blocks/EC2 Instance/Ec2_instance.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws={
+        source = "hashicorp/aws"
+        version = "> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = ap-south-1
+  
+}
+
+resource "aws_instance" "EC2_instance" {
+     ami = "ami-0522ab6e1ddcc7055"
+     instance_type = "t2.micro"
+     tags = {
+       Name = "terraform_demo"
+     }
+}
