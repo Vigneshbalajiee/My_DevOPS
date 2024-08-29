d5c24cbf6389d035b6c9cfc5331d4a024af7013b
vignesh <vigneshbalajiee15@gmail.com>
2024-08-29 19:31:58 +0530

    commit

--- Terraform/Meta-arguments/ec2instance/SG.tf
+++ Terraform/Meta-arguments/ec2instance/SG.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "my_SG_2" {
         Name = "my_SG_2"
     }
 dynamic "ingress"{
-for_each = [ local.ingress ]
+for_each = local.ingress
 content {
     from_port = ingress.value.port
     to_port = ingress.value.port
